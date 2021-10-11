1bd8840f95bf19d369cf7df193f2091289b538ea
Jack McNary <jack.mcnary@gmail.com>
2021-10-11 12:03:43 -0600

    Final upload, prior to tweaking links for the live url

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,9 +7,10 @@ var city = "";
 searchedCity = document.querySelector("#search-city");
 currentWeatherContainer = document.querySelector("#current-weather");
 fiveDayContainer = document.querySelector("#five-day-container");
-recentSearches = document.querySelector("#recent-searches");
+recentSearches = document.querySelector("recent-searches");
 
 localStorage.getItem("Stored City");
+// function to grab the latitude and longitude
 function getLatLon(cityInput) {
     // var city = localStorage.getItem("city");
     var searchedCity = localStorage.getItem("cities");
@@ -30,22 +31,58 @@ function getLatLon(cityInput) {
                     var units = "&units=imperial";
                     var newSearch = apiUrlOneCall + "lat=" + latitude + "&lon=" + longitude + units + exclude + "&appid=" + APIKey;
                     console.log(newSearch);
+                    dataObj = data;
+                    console.log(dataObj);
+                    localStorage.setItem("OneCallUrl", newSearch);
                     console.log(data);
 
-                    displayWeather(newSearch);
+
+                    fiveDayForecast(newSearch);
 
                 });
-            } else {
-                alert('Error: ' + response.statusText);
             }
-
-        })
-
-    uvIndex();
+        });
 }
 
+// function to get the current weather from the Open Weather Map api
+function currentWeather() {
+    // let city = $('#search-city').val();
+    searchedCity = $('#search-city').val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial" + "&appid=" + APIKey;
+    fetch(queryURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+            // establishing the variables to be displayed on the card
+            let weatherIcon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            let currentTimeUTC = response.dt;
+            let currentTimeZoneOffset = response.timezone;
+            let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+            let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+            $('#header-text').text(response.name);
 
-function displayWeather(newSearch) {
+            // using a template literal to display the card's text content
+            let currentWeatherHTML = `
+            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${weatherIcon}"></h3>
+            <div class="list-unstyled">
+                <div>Temperature: ${response.main.temp}&#8457;</div>
+                <div>Humidity: ${response.main.humidity}%</div>
+                <div>Wind Speed: ${response.wind.speed} mph</div>
+                <div class="uvIndex">UV Index: ${response.wind.speed}</div>
+            </div>`;
+            // <div id="uvIndex">UV Index: ${uvIndex}</div>
+            // UVbtn.textContent = data.current.uvi;
+            // console.log(data.current.uvi);
+            $('#current-weather').html(currentWeatherHTML);
+
+        })
+}
+
+// function to display the five-day forecast for an area
+// in retrospect, I could have used a template literal for this section to, and it would make sense to do that in a refactor. For now, it works, so I'm not going to mess with it.
+function fiveDayForecast(newSearch) {
     fetch(newSearch)
         .then(function (response) {
             return response.json();
@@ -54,8 +91,9 @@ function displayWeather(newSearch) {
             console.log(newSearch);
             dataObj = data;
             console.log(dataObj);
-
+            let uvIndex = dataObj.current.uvi;
             for (var i = 1; i < dataObj.daily.length - 2; i++) {
+                console.log(dataObj.daily.length);
                 var weatherCard = document.createElement('div');
                 weatherCard.classList.add('col-2', 'card', 'weatherCard');
                 weatherCard.style.marginLeft = '25px';
@@ -86,25 +124,11 @@ function displayWeather(newSearch) {
                 weatherBody.append(dateEl, weatherIcon, weatherList);
                 weatherCard.append(weatherBody);
                 fiveDayContainer.append(weatherCard);
+                // currently prints the 5-day weather forecast twice with repeat searches. Definitely something to handle in the refactor
 
             }
-        });
-}
-
-function uvIndex() {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial" + "&appid=" + APIKey;
-    // fetch(queryURL1)
-    // let latitude = localStorage.getItem("Longitude");
-    // let longitude = localStorage.getItem("Latitude");
-    // let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
-    // console.log(queryURL);
-    fetch(queryURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((response) => {
-            console.log(response);
-            let uvIndex = response.value;
+            // way to display the uv content
+            uvIndex.textContent = data.current.uvi;
             $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
             if (uvIndex >= 0 && uvIndex < 3) {
                 $('#uvVal').attr("class", "uv-favorable");
@@ -113,62 +137,40 @@ function uvIndex() {
             } else if (uvIndex >= 8) {
                 $('#uvVal').attr("class", "uv-severe");
             }
+
+
         });
 }
 
-// created function to get the current weather in a given city from the OWM API
-function currentWeather() {
-    // let city = $('#search-city').val();
-    searchedCity = $('#search-city').val();
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial" + "&appid=" + APIKey;
-    fetch(queryURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((response) => {
-            console.log(response);
-            let weatherIcon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-            let currentTimeUTC = response.dt;
-            let currentTimeZoneOffset = response.timezone;
-            let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
-            let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
-            $('#header-text').text(response.name);
 
 
-            let currentWeatherHTML = `
-            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${weatherIcon}"></h3>
-            <div class="list-unstyled">
-                <div>Temperature: ${response.main.temp}&#8457;</div>
-                <div>Humidity: ${response.main.humidity}%</div>
-                <div>Wind Speed: ${response.wind.speed} mph</div>
-                <div class="uvIndex">UV Index: ${response.value}</div>
-            </div>`;
-            // <div id="uvIndex">UV Index: ${uvIndex}</div>
-            // UVbtn.textContent = data.current.uvi;
-            // console.log(data.current.uvi);
-            $('#current-weather').html(currentWeatherHTML);
-
-        })
-}
-
-function getPastSearch() {
-    localStorage.getItem("Stored Cities")
-    $('#recent-searches').append(previousSearch)
-
-
-
-}
+// adding the searched city to local storage
 function addCity(storedCity) {
     storedCity = localStorage.getItem("cities");
     {
-        var previousSearch = $('<div>').text(storedCity);
+        var previousSearch = $('<button>').text(storedCity)
         $('#recent-searches').append(previousSearch)
-        localStorage.setItem("Stored Cities", JSON.stringify(previousSearch));
+        previousSearch.setAttribute("class", "row");
 
     }
 
 }
+// not working exactly as I had intended, but I wanted to make each past entry searchable 
+$('#choose-recent-searches').on('click'), "button", function () {
+    getLatLon($(this).text())
+    localStorage.setItem('city', $(this).text())
 
+}
+// related to the function above
+function previousSearchBtn(storedCity) {
+    var previousSearch = $('<button>').addClass('btn btn-secondary col-12').text(storedCity).css("margin-bottom", "10px")
+    $('#choose-recent-searches').append(previousSearch)
+}
+
+// calling the function, not sure exactly what variable to put in here
+previousSearchBtn(storedCity);
+
+// event listener to search for a city - starts the other functions getLatLon and addCity
 $('#search-button').on("click", (event) => {
     event.preventDefault();
     searchedCity = $('#search-city').val();
@@ -176,10 +178,13 @@ $('#search-button').on("click", (event) => {
     currentWeather(searchedCity);
     localStorage.setItem("cities", searchedCity)
     getLatLon();
+
     addCity();
+
 
 });
 
+// function to clear the history and reload the page
 $('#clear-history').on('click', (event) => {
     event.preventDefault();
     localStorage.removeItem("cities");
